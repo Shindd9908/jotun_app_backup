3274e1e2e8020730d94b74098724c1cbc4c32a3f
hapo-nghialt <nghia.lt@haposoft.com>
2024-03-20 00:59:37 +0700

    resolve conflict

---
+++ lib/features/home/presentation/ui/screens/home_screen.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/image_main_background.png'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 24.h,
+                horizontal: 34.w,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/icons/icon_avatar.png',
+                        width: 40.w,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'HOÀNG VĂN A',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.sp,
+                              height: 1.sp,
+                              letterSpacing: 0.1,
+                            ),
+                          ),
+                          Text(
+                            'Nhóm 10',
+                            style: TextStyle(
+                              color: const Color(0xffE1AA40),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.sp,
+                              height: 1.sp,
+                              letterSpacing: 0.1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    'assets/icons/icon_notification.png',
+                    width: 40.w,
+                  )
+                ],
+              ),
+            ),
+            Image.asset('assets/images/image_banner.png'),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 28.w,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 18.h,
+                      bottom: 12.h,
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Sự kiện sắp diễn ra',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.sp,
+                          letterSpacing: 0.05,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(12.r),
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.white,
+                      ),
+                    ),
+                    padding: EdgeInsets.only(
+                      top: 20.h,
+                      left: 8.w,
+                      right: 8.w,
+                      bottom: 4.h,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'LỄ KỶ NIỆM 20 NĂM\nNGÀNH SƠN TRANG TRÍ JOTUN VIỆT NAM',
+                          style: TextStyle(
+                            color: const Color(0xffF19F3B),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.sp,
+                            letterSpacing: -0.9,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 18.h,
+                            bottom: 18.h,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Image.asset(
+                                    'assets/icons/icon_calendar.png',
+                                    width: 24.w,
+                                  ),
+                                  SizedBox(width: 4.w),
+                                  Text(
+                                    '20:00',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.sp,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Image.asset(
+                                    'assets/icons/icon_calendar.png',
+                                    width: 24.w,
+                                  ),
+                                  SizedBox(width: 4.w),
+                                  Text(
+                                    'Hội trường Ariyana',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.sp,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          'Yêu cầu sự kiện: Lễ phục, trang phục dự tiệc',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.sp,
+                          ),
+                        ),
+                        SizedBox(height: 32.h),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Xem chi tiết hành trình',
+                              style: TextStyle(
+                                color: const Color(0xffF19F3B),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12.sp,
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: const Color(0xffF19F3B),
+                              size: 12.sp,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 12.h,
+                      bottom: 12.h,
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Tính năng',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.sp,
+                          letterSpacing: 0.05,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
